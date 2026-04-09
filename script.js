938f5f5c69a8411afa3acf953612670d06b16972
santosh <homepageiitb@gmail.com>
2026-04-09 16:35:50 +0530

    demo

---
+++ script.js
@@ -0,0 +1,20 @@
+function searchStaff() {
+
+let input = document.getElementById("searchBox").value.toLowerCase();
+
+let cards = document.getElementsByClassName("card");
+
+for (let i = 0; i < cards.length; i++) {
+
+let text = cards[i].innerText.toLowerCase();
+
+if (text.includes(input)) {
+cards[i].style.display = "block";
+} 
+else {
+cards[i].style.display = "none";
+}
+
+}
+
+}
